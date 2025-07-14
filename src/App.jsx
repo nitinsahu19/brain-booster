1f0b88c2ce8dd9b1a50a7406654fb1a2a47e444d
Nitin Sahu <sahunitin900@gmail.com>
2025-07-14 13:57:22 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h2 className="h-screen w-full flex justify-center items-center">
+        Hello students
+      </h2>
+    </>
+  );
+}
+
+export default App;
